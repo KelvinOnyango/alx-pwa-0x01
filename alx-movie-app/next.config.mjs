0d9525dc04edb04a5e9b03746e9a7bd49eb30c5f
Kelvin Onyango <kelvinonyango155@gmail.com>
2025-07-05 21:36:17 -0700

    your commit message

---
+++ alx-movie-app/next.config.mjs
@@ -0,0 +1,19 @@
+// next.config.mjs
+import withPWAInit from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["m.media-amazon.com"],
+  },
+};
+
+export default withPWA(nextConfig);
--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,10 +1,20 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
